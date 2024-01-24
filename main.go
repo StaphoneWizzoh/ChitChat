a6f8dd5d58f67271ed1214a9c5beee99fe48f466
Staphone <staphoneomondi@gmail.com>
2024-01-24 12:49:33 +0300

    Basic Chat Functionality

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,25 +10,37 @@ import (
 )
 
 // templ represents a single template
-type templateHandler struct {
+type TemplateHandler struct {
 	once sync.Once
 	filename string
 	templ *template.Template
    }
    // ServeHTTP handles the HTTP request.
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (t *TemplateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
    }
 
 func main(){
-	// root
-	http.Handle("/", &templateHandler{filename: "chat.html"})
-	// start the web server
-	log.Println("Server running on http://localhost:8000")
-	if err := http.ListenAndServe(":8000", nil); err != nil {
+	var addr = flag.String("addr", ":8000", "The addr of the application.")
+	flag.Parse()
+
+	r := newRoom()
+	http.Handle("/", &TemplateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// Starting the room
+	go r.run()
+
+	// Serving static files from the "static" subdirectory
+	staticHandler := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+
+	// Starting the web server
+	log.Printf("Server started at port http://localhost%s",*addr )
+	if err := http.ListenAndServe(*addr, nil); err != nil{
 		log.Fatal("ListenAndServe:", err)
 	}
 }
